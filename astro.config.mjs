f0f971cc36051a0e59bfb8e5e60ccd18530e861e
Marjukka Niinioja <mniinio@users.noreply.github.com>
2026-07-19 12:45:34 +0300

    Migrate production scaffold to static Astro

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
+
+const site =
+  process.env.PUBLIC_SITE_ORIGIN ??
+  process.env.NEXT_PUBLIC_SITE_ORIGIN ??
+  process.env.SITE_ORIGIN ??
+  "https://beta.apiopscycles.com";
+
+export default defineConfig({
+  output: "static",
+  site,
+  publicDir: "./public",
+  outDir: "./dist",
+  integrations: [react()],
+});
